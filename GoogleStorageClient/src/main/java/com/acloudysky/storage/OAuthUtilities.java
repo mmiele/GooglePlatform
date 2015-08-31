cc1d15584097cec2d73521c4423673bf0e18c3ba
mmiele <mielem@gmail.com>
2015-08-31 15:18:25 -0700

    Added comments

--- GoogleStorageClient/src/main/java/com/acloudysky/storage/OAuthUtilities.java
+++ GoogleStorageClient/src/main/java/com/acloudysky/storage/OAuthUtilities.java
@@ -3,10 +3,24 @@ package com.acloudysky.storage;
 import com.acloudysky.oauth.AuthorizedService;
 import com.google.api.services.storage.Storage;
 
-
+/**
+ * This class contains utility methods to handle authorization tasks.
+ * @author michael
+ *
+ */
 public class OAuthUtilities {
 	
-	public static Storage getAuthorizedService(String serviceScopes) {
+	/***
+	 * Authorizes the application to use Google Storage API. 
+	 * <p><b>Note</b>. The method assumes that you already have created a directory to 
+	 * store the file with the client secrets. The directory is .store/storage_sample. 
+	 * The file containing the secrets is client_secrets.json.
+	 * @param serviceScope - The scope for which to obtain the authorization.
+	 * The values for the scope can be found at 
+	 * <a href="https://developers.google.com/resources/api-libraries/documentation/storage/v1beta2/java/latest/com/google/api/services/storage/StorageScopes.html" target="_blank">Class StorageScopes</a>.
+	 * @return The object that represents the authorized service.
+	 */
+	public static Storage getAuthorizedService(String serviceScope) {
 		
 		Storage storageService = null;
 				
@@ -17,7 +31,7 @@ public class OAuthUtilities {
 					new AuthorizedService(".store", "storage_sample", "client_secrets.json");
 		
 			// Get the authorized service so the application can use its API.
-			storageService = (Storage) service.getAuthorizedService("storage", serviceScopes);
+			storageService = (Storage) service.getAuthorizedService("storage", serviceScope);
 			
 		}
 		catch (Exception e) {
